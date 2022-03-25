90981b103178a7562f917ad470409174e35044f6
yaarhhl <yaarhhl@mail.ru>
2022-03-25 11:09:17 +0300

    2
    test2

--- app/src/main/java/com/example/main_project_android/BasketActivity.java
+++ app/src/main/java/com/example/main_project_android/BasketActivity.java
@@ -3,7 +3,13 @@ package com.example.main_project_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import com.example.main_project_android.classes.Basket;
+import com.example.main_project_android.classes.Product;
+
+import java.util.List;
 
 public class BasketActivity extends AppCompatActivity {
 
@@ -20,6 +26,10 @@ public class BasketActivity extends AppCompatActivity {
 //        totalPrice += i;
 //        tvTotalPrice.setText(totalPrice);
 
+
+    }
+
+    public void pay_for_the_shopping_cart(View view) {
 
     }
 }
--- app/src/main/java/com/example/main_project_android/MainActivity_Catalog.java
+++ app/src/main/java/com/example/main_project_android/MainActivity_Catalog.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.main_project_android.classes.Basket;
 import com.example.main_project_android.classes.Catalog;
 import com.example.main_project_android.classes.Product;
 
@@ -14,6 +16,7 @@ public class MainActivity_Catalog extends Activity {
 
 
     ListView lv_catalog;
+    Button goToBasketButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,7 @@ public class MainActivity_Catalog extends Activity {
         setContentView(R.layout.product_catalog);
 
         lv_catalog = findViewById(R.id.product_list);
+        goToBasketButton = findViewById(R.id.goToBasket);
         ProductCatalogAdapter adapter = new ProductCatalogAdapter(this, makeProduct());
         lv_catalog.setAdapter(adapter);
 
@@ -53,5 +57,8 @@ public class MainActivity_Catalog extends Activity {
         Intent i = new Intent(MainActivity_Catalog.this, ProductActivity.class);
         i.putExtra("nd", str);
         startActivity(i);
+    }
+    public void goToBasket(View view) {
+        //new Basket()
     }
 }
